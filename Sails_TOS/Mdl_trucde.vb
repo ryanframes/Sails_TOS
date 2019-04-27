6c65959fc2ceedecbc4770cecbf219965f5175e6
ryanframes <xmozart.ryan@gmail.com>
2019-04-27 12:41:27 +0700

    fn_trucde_getdata

--- Sails_TOS/Mdl_trucde.vb
+++ Sails_TOS/Mdl_trucde.vb
@@ -1,6 +1,20 @@
 ﻿Module Mdl_trucde
     Function fc_trucde_getdata(ByVal apiAddress As String, Optional apiCondition As String = Nothing) As DataTable
-
+        Dim sCol As New Dictionary(Of String, String)
+        '{"TRUCK_ID", "TID", "Truck Number", "Status", "RFID Code", "Last In Date",
+        '   "Last Out Date", "ITV Flag", "Trucking Company", "Remarks"}
+        'SETTING COL
+        sCol.Add("TRUCK_ID", "")
+        sCol.Add("TID", "")
+        sCol.Add("TRUCK_NUMBER", "Truck Number")
+        sCol.Add("STATUS", "Status")
+        sCol.Add("RFID_CODE", "RFID Code")
+        sCol.Add("LAST_IN_DATE", "Last In Date")
+        sCol.Add("LAST_OUT_DATE", "Last Out Date")
+        sCol.Add("ITV_FLAG", "ITV Flag")
+        sCol.Add("TRUCKING_COMPANY", "Trucking Company")
+        sCol.Add("REMARKS", "Remarks")
+        Dim dt As DataTable = getDataFromAPI(apiAddress, "rts_trucde_getdata", sCol, apiCondition)
     End Function
 
     Function fc_trucde_savedata(tid As String, truck_number As String, rfid As String,
